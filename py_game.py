39929899aa9ddf18bcad1c0577c0f8f0bbdb5fc3
chodec <dom.lepizmo@email.cz>
2024-06-30 00:36:04 +0200

    add levels

--- py_game.py
+++ py_game.py
@@ -4,6 +4,7 @@ pygame.init()
 
 BLACK = (0, 0, 0)
 CRIMSON = (220, 20, 60)
+LIGHT_GREEN = (124, 252, 0)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 PLAYER_SPEED = 2 
@@ -15,6 +16,9 @@ SHOOT_COOLDOWN = 1000
 ENEMY_SPAWN_COOLDOWN = 2500
 HEALTH_BAR_WIDTH = 200
 HEALTH_BAR_HEIGHT = 20
+XP_BAR_WIDTH = 200
+XP_BAR_HEIGHT = 20
+FONT_SIZE = 24
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Scott Penguin vs World')
@@ -34,6 +38,11 @@ def get_projectile_sprite(sheet, col, row, width, height):
     image.blit(sheet, (0, 0), (col * width, row * height, width, height))
     return image
 
+def get_item_tile(sheet, col, row, width, height):
+    image = pygame.Surface((width, height), pygame.SRCALPHA)
+    image.blit(sheet, (0, 0), (col * width, row * height, width, height))
+    return image
+
 grass_tile = get_tile(sprite_sheet, 0, 15, TILE_SIZE, TILE_SIZE)
 road_tile_1 = get_tile(sprite_sheet, 7, 15, TILE_SIZE, TILE_SIZE)
 road_tile_2 = get_tile(sprite_sheet, 9, 15, TILE_SIZE, TILE_SIZE)
@@ -45,6 +54,8 @@ projectile_up = get_projectile_sprite(sprite_sheet, 44, 10, TILE_SIZE, TILE_SIZE
 projectile_right = get_projectile_sprite(sprite_sheet, 46, 10, TILE_SIZE, TILE_SIZE)
 projectile_down = get_projectile_sprite(sprite_sheet, 48, 10, TILE_SIZE, TILE_SIZE)
 projectile_left = get_projectile_sprite(sprite_sheet, 50, 10, TILE_SIZE, TILE_SIZE)
+
+exp_tile = get_item_tile(sprite_sheet, 1, 10, TILE_SIZE, TILE_SIZE)
 
 map_layout = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
@@ -76,6 +87,7 @@ class Projectile:
         self.y = y
         self.speed = 5
         self.direction = direction
+        self.alive = True 
         if direction == "up":
             self.image = projectile_up
         elif direction == "right":
@@ -96,7 +108,20 @@ class Projectile:
             self.y += self.speed
 
     def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
+        if self.alive:
+            screen.blit(self.image, (self.x, self.y))
+
+    def destroy(self):
+        self.alive = False
+
+class Item:
+    def __init__(self, x, y, sprite):
+        self.x = x
+        self.y = y
+        self.sprite = sprite
+
+    def draw(self, screen):
+        screen.blit(self.sprite, (self.x, self.y))
 
 class Player:
     def __init__(self, x, y, sprite_sheet):
@@ -108,6 +133,9 @@ class Player:
         self.frame_counter = 0
         self.direction = "right"
         self.health = 100
+        self.xp = 0
+        self.level = 1  # Initialize level
+        self.projectile_count = 1  # Initialize projectile count
         self.right_sprites = [self.get_sprite(i, 0) for i in range(7)]
         self.left_sprites = [pygame.transform.flip(sprite, True, False) for sprite in self.right_sprites]
         self.up_sprites = [self.get_sprite(i, 1) for i in range(7)]
@@ -151,12 +179,24 @@ class Player:
         for bullet in self.bullets:
             bullet.update()
 
+    def check_level_up(self):
+        if self.xp >= 100:
+            self.level += 1
+            self.xp = 0
+            self.projectile_count += 1  # Increase the number of projectiles to shoot
+
+    def collect_item(self, item):
+        self.xp += 10
+        self.check_level_up()
+
     def draw(self, screen):
         sprite = self.current_sprites[self.current_frame]
         screen.blit(sprite, (self.x, self.y))
         for bullet in self.bullets:
             bullet.draw(screen)
         self.draw_health_bar(screen)
+        self.draw_xp_bar(screen) 
+        self.draw_level(screen)
 
     def draw_health_bar(self, screen):
         bar_x = SCREEN_WIDTH - HEALTH_BAR_WIDTH - 10
@@ -164,15 +204,41 @@ class Player:
         pygame.draw.rect(screen, BLACK, (bar_x, bar_y, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT), 2)
         fill_width = (self.health / 100) * (HEALTH_BAR_WIDTH - 4)
         pygame.draw.rect(screen, CRIMSON, (bar_x + 2, bar_y + 2, fill_width, HEALTH_BAR_HEIGHT - 4))
+        font = pygame.font.SysFont(None, 24)
+        hp_text = font.render('HP', True, BLACK)
+        screen.blit(hp_text, (bar_x - 30, bar_y))
+    
+    def draw_xp_bar(self, screen):
+        bar_x = 40
+        bar_y = SCREEN_HEIGHT - XP_BAR_HEIGHT - 10
+        pygame.draw.rect(screen, BLACK, (bar_x, bar_y, XP_BAR_WIDTH, XP_BAR_HEIGHT), 2)
+        fill_width = (self.xp / 100) * (XP_BAR_WIDTH - 4)
+        pygame.draw.rect(screen, LIGHT_GREEN, (bar_x + 2, bar_y + 2, fill_width, XP_BAR_HEIGHT - 4))
+        font = pygame.font.SysFont(None, 24)
+        xp_text = font.render('XP', True, BLACK)
+        screen.blit(xp_text, (bar_x - 30, bar_y))
+
+    def draw_level(self, screen):
+        font = pygame.font.SysFont(None, 24)
+        level_text = font.render(f'Level {self.level}', True, BLACK)
+        text_rect = level_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 30))
+        screen.blit(level_text, text_rect)
 
     def shoot(self):
         current_time = pygame.time.get_ticks()
         if current_time - self.last_shot_time >= SHOOT_COOLDOWN:
-            bullet_x = self.x + TILE_SIZE // 2 * SCALE_PLAYER
-            bullet_y = self.y + TILE_SIZE // 2 * SCALE_PLAYER
-            new_bullet = Projectile(bullet_x, bullet_y, self.direction)
-            self.bullets.append(new_bullet)
+            bullets = []
+            for i in range(self.projectile_count):
+                bullet_x = self.x + TILE_SIZE // 2 * SCALE_PLAYER
+                bullet_y = self.y + TILE_SIZE // 2 * SCALE_PLAYER
+                new_bullet = Projectile(bullet_x, bullet_y, self.direction)
+                bullets.append(new_bullet)
+                self.bullets.append(new_bullet)
             self.last_shot_time = current_time
+            return bullets
+        return None
+
+
 
 class Enemy:
     def __init__(self, x, y, health, damage, speed):
@@ -197,6 +263,10 @@ class Enemy:
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
 
+    def drop_item(self):
+        return Item(self.x, self.y, exp_tile)
+
+
 class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -205,6 +275,7 @@ class Game:
         self.sprite_sheet = pygame.image.load('penguin.png').convert_alpha()
         self.player = Player(400, 300, self.sprite_sheet)
         self.enemies = []
+        self.items = []  # List to hold items
         self.last_enemy_spawn_time = pygame.time.get_ticks()
         self.running = True
 
@@ -235,31 +306,64 @@ class Game:
             self.enemies.append(new_enemy)
             self.last_enemy_spawn_time = current_time
 
+    def check_collisions(self):
+        for item in list(self.items):
+            if self.player.x < item.x + TILE_SIZE and self.player.x + TILE_SIZE > item.x and \
+               self.player.y < item.y + TILE_SIZE and self.player.y + TILE_SIZE > item.y:
+                self.player.collect_item(item)
+                self.items.remove(item)
+
     def run(self):
+        self.projectiles = []
+
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
-                        self.player.shoot()
+                        projectile = self.player.shoot()
+                        if projectile:  # Only add if a projectile was actually created
+                            self.projectiles.append(projectile)
 
             keys = pygame.key.get_pressed()
             self.player.update(keys)
             self.spawn_enemy()
-            for enemy in self.enemies:
+
+            for projectile in list(self.projectiles):
+                projectile.update()
+                if not projectile.alive:
+                    self.projectiles.remove(projectile)
+
+            for enemy in list(self.enemies):
                 enemy.update(self.player)
+                for projectile in list(self.projectiles):
+                    dist = math.hypot(enemy.x - projectile.x, enemy.y - projectile.y)
+                    if dist < TILE_SIZE:
+                        self.enemies.remove(enemy)
+                        item = enemy.drop_item()
+                        self.items.append(item)
+                        projectile.destroy()
+                        break
+
+            self.check_collisions()  # Check for collisions with items
 
             self.screen.fill(BLACK)
             self.draw_map()
             self.player.draw(self.screen)
             for enemy in self.enemies:
                 enemy.draw(self.screen)
+            for item in self.items:
+                item.draw(self.screen)
+            for projectile in self.projectiles:
+                projectile.draw(self.screen)
             pygame.display.flip()
 
             self.clock.tick(FPS)
 
         pygame.quit()
+
+
 
 if __name__ == "__main__":
     game = Game()
